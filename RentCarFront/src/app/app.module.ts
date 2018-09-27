393f5ad7f7711fa358e8061887e7ff2674848749
nagulapallinaveen <naveen.kumar.satya@gmail.com>
2018-09-27 17:07:27 +0530

    Integrated v1

--- RentCarFront/src/app/app.module.ts
+++ RentCarFront/src/app/app.module.ts
@@ -1,18 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import { MatNativeDateModule,MatButtonModule, MatCheckboxModule, MatSelectModule, MatInputModule, MatCardModule, MatStepperModule,  MatDatepickerModule} from '@angular/material';
+import { MatToolbarModule, MatNativeDateModule,MatButtonModule, MatCheckboxModule, MatSelectModule, MatInputModule, MatCardModule, MatStepperModule,  MatDatepickerModule} from '@angular/material';
 import 'hammerjs';
-
+import {RouterModule, Routes} from '@angular/router';
 import { AppComponent } from './app.component';
-import { CarDetailsComponent } from './car-details/car-details.component';
+import { CarDetailsComponent } from './components/car-details/car-details.component';
 import { LoginComponent } from './components/login/login.component';
 import { SignupComponent } from './components/signup/signup.component';
 
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { GetDetailsComponent } from './components/get-details/get-details.component';
-
+import { HomeComponent } from './components/home/home.component';
+const routes: Routes=[
+{path:'home', component:HomeComponent},
+{path:'login',component:LoginComponent},
+{path:'signup',component:SignupComponent},
+{path:'get-details',component:GetDetailsComponent}
+];
 @NgModule({
   declarations: [
     AppComponent,
@@ -20,6 +26,7 @@ import { GetDetailsComponent } from './components/get-details/get-details.compon
     LoginComponent,
     SignupComponent,
     GetDetailsComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
@@ -37,6 +44,8 @@ import { GetDetailsComponent } from './components/get-details/get-details.compon
     ReactiveFormsModule,
     MatDatepickerModule,
     MatNativeDateModule,
+    MatToolbarModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
--- RentCarFront/src/app/components/login/login.component.ts
+++ RentCarFront/src/app/components/login/login.component.ts
@@ -1,4 +1,5 @@
 import {Component,OnInit} from '@angular/core';
+import { SignupComponent } from '../signup/signup.component';
 
 /**
  * @title Basic Inputs
